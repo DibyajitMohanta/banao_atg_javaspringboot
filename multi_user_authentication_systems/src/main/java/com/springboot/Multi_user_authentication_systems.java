496e2e608d8cda27e1684cab083f150534ac94c3
Dibyajit Mohanta <dibyajit1920@gmail.com>
2023-07-07 23:30:44 +0530

    first commit

---
+++ multi_user_authentication_systems/src/main/java/com/springboot/Multi_user_authentication_systems.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Multi_user_authentication_systems {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Multi_user_authentication_systems.class, args);
+	}
+
+}
